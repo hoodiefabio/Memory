5dccaedc5f865bce8b3e2698b91c721d6c3fdecf
hoodiefabio <73169440+hoodiefabio@users.noreply.github.com>
2023-06-02 13:53:18 +0200

    Applied more feedback

--- Assets/Scripts/MusicOverflow.cs
+++ Assets/Scripts/MusicOverflow.cs
@@ -5,13 +5,30 @@ using UnityEngine;
 public class MusicOverflow : MonoBehaviour
 {
     private AudioSource bgm;
+    private GameObject[] other;
     public bool muted = false;
-    
+    private bool NotFirst = false;
+
     private void Awake()
     {
-        DontDestroyOnLoad(gameObject);
+        other = GameObject.FindGameObjectsWithTag("Music");
+
+        foreach (GameObject oneOther in other)
+        {
+            if (oneOther.scene.buildIndex == -1)
+            {
+                NotFirst = true;
+            }
+        }
+
+        if (NotFirst == true)
+        {
+            Destroy(gameObject);
+        }
+        DontDestroyOnLoad(transform.gameObject);
         bgm = GetComponent<AudioSource>();
     }
+
 
     public void PlayMusic()
     {
